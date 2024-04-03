9ec38e8a45c5eaf0b38a330a4a75e7e48a045a0c
rw610 <eggoru@gmail.com>
2024-04-03 05:47:26 +0300

    ...

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
 def rec_mul(num1: int, num2: int) -> int:
-	... 
+	n = max(len(str(num1)), len(str(num2)))
+	b, a = int(str(num1)[:n//2]), int(str(num1)[n//2:])
+	d, c = int(str(num2)[:n//2]), int(str(num2)[n//2:])
+	print(a, b, c, d)
+
+rec_mul(123, 1234) 

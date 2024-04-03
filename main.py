aa8c9495a930553f91b79615d1c68aebd2f97257
rw610 <eggoru@gmail.com>
2024-04-03 05:57:39 +0300

    done

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
-def rec_mul(num1: int, num2: int) -> int:
-	n = max(len(str(num1)), len(str(num2)))
-	b, a = int(str(num1)[:n//2]), int(str(num1)[n//2:])
-	d, c = int(str(num2)[:n//2]), int(str(num2)[n//2:])
-	print(a, b, c, d)
+def karatsuba(num1: int, num2: int) -> int:
+    if num1 < 10 or num2 < 10:
+        return num1 * num2
+    
+    n = max(len(str(num1)), len(str(num2)))
+    half = n // 2
+    
+    a, b = divmod(num1, 10 ** half)
+    c, d = divmod(num2, 10 ** half)
 
-rec_mul(123, 1234) 
+    ac = karatsuba(a, c)
+    bd = karatsuba(b, d)
+    ad_bc = karatsuba(a + b, c + d) - ac - bd
+    
+    return (ac * (10 ** (2 * half))) + (ad_bc * (10 ** half)) + bd
+
+
+mul = karatsuba(10, 100) 
+print(mul)
